54149a27e50fbb9fb009bc55a4a999c18664cd4d
Ben Hadfield <benjohnhadfield@gmail.com>
2017-11-14 15:06:18 +0000

    🌱

---
+++ api/settings/prod.py
@@ -0,0 +1,13 @@
+import dj_database_url
+
+from .common import *
+
+
+# Update database config with $DATABASE_URL
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
